f1ec5cac4d47191f2ae60a29787621038091f76b
MarlonDougs <marlondouglas2@hotmail.com>
2023-10-02 22:49:48 -0300

    ListaTarefa DIO

---
+++ src/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,24 @@
+package list.OperacoesBasicas;
+
+public class Tarefa {
+    //atributo
+    private String descricao;
+
+
+    Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    @Override
+    public String toString() {
+        return descricao;
+    }
+    
+}
+
+
